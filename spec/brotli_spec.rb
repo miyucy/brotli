cd59a0be94088a1f312c8f406b5348731fb7e11b
miyucy <fistfvck@gmail.com>
2015-12-29 12:12:37 +0900

    add tests

--- spec/brotli_spec.rb
+++ spec/brotli_spec.rb
@@ -6,9 +6,9 @@ describe Brotli do
     expect(Brotli::VERSION).not_to be nil
   end
 
-  let(:data) { File.read(__FILE__, mode: 'rb') }
+  let(:data) { File.binread __FILE__ }
 
-  it 'works' do
+  it 'deflate/inflate' do
     compressed = Brotli.deflate(data)
     decompressed = Brotli.inflate(compressed)
     expect(decompressed).to eq data
@@ -20,5 +20,11 @@ describe Brotli do
 
     compressed = Brotli.deflate(data)
     puts "Brotli compress ratio: #{(compressed.bytesize / data.bytesize.to_f * 100).round(3)} %"
+  end
+
+  it 'inflate' do
+    file = File.binread File.join(__dir__, '..', 'vendor', 'brotli', 'tests', 'testdata', 'alice29.txt')
+    compressed = File.binread File.join(__dir__, '..', 'vendor', 'brotli', 'tests', 'testdata', 'alice29.txt.compressed')
+    expect(Brotli.inflate compressed).to eq file
   end
 end
